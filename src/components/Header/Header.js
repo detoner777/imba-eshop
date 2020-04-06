ab3de004484e9eaa9110ad3763a808f96ca55312
Alexey_Lisenko <detoner777@gmail.com>
2020-04-06 13:13:35 +0300

    navbar layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,19 +8,56 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Badge from "react-bootstrap/Badge";
 
+import logo from "../../assets/logo.svg";
+import shoping_cart from "../../assets/icons/shopping_cart.png";
+
 const Header = () => {
   return (
     <Navbar
       collapseOnSelect
       expand="lg"
-      bg="primary"
-      variant="dark"
-      fixed="top"
+      bg="light"
+      variant="light"
+      sticky="top"
     >
       <Container>
-        <span>22</span>
+        <Navbar.Brand href="#home">
+          <img src={logo} className="logo" alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav className="nav-pages">
+              {" "}
+              <Nav.Link href="/home">Главная</Nav.Link>
+              <Nav.Link href="/products">Магазин</Nav.Link>
+              <Nav.Link href="/victory">Розыгрыш!</Nav.Link>
+              <Nav.Link href="/contacts">Контакты</Nav.Link>
+            </Nav>
+
+            <Nav.Link>
+              <img src={shoping_cart} alt="shoping-cart" id="shoping_cart" />{" "}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
+
+    // <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" sticky="top">
+    //   <Container>
+    //     <Navbar.Brand href="#home">
+    //       <img src={logo} className="logo" alt="logo" />
+    //     </Navbar.Brand>
+    //     <Nav.Link href="/home">Главная</Nav.Link>
+    //     <Nav.Link href="/products">Магазин</Nav.Link>
+    //     <Nav.Link href="#features">Розыгрыш!</Nav.Link>
+    //     <Nav.Link href="#features">Контакты</Nav.Link>
+
+    //     <Nav.Link>
+    //       <i className="fa fa-shopping-cart"></i> Cart{" "}
+    //     </Nav.Link>
+    //   </Container>
+    // </Navbar>
   );
 };
 
