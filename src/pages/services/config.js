6a5a53cca5887828d423969742547ac07cb2fcd1
Alexey_Lisenko <detoner777@gmail.com>
2020-04-11 13:58:01 +0300

    action+reducers init

---
+++ src/pages/services/config.js
@@ -0,0 +1,7 @@
+export const config = {
+  baseURL: "http://localhost3000",
+  products: "/products",
+  pageSize: 10,
+  advertIndex: 5,
+  delay: 0,
+};
---
+++ src/pages/store/reducers/productsReducer.js
@@ -0,0 +1,53 @@
+import * as Types from "../types";
+import { config } from "../../services/config";
+
+const initialState = {
+  isLoading: false,
+  error: null,
+  products: [],
+  hasMoreItems: true,
+};
+
+const productsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case Types.LOAD_PRODUCTS_INIT:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case Types.LOAD_PRODUCTS_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        hasMoreItems: false,
+        error: payload,
+      };
+    case Types.LOAD_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        hasMoreItems: payload.length > config.pageSize,
+        error: null,
+        products: payload,
+      };
+    case Types.LOAD_MORE_PRODUCTS_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        hasMoreItems: false,
+        error: payload,
+      };
+    case Types.LOAD_MORE_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        hasMoreItems: payload.length > config.pageSize,
+        error: null,
+        products: [...state.products, ...payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default productsReducer;
