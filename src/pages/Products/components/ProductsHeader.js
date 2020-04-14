a5b7c567056948be1b8dcf20a12106ad65a46b9e
Alexey_Lisenko <detoner777@gmail.com>
2020-04-14 20:17:26 +0300

    ProductsHeader component layout

---
+++ src/pages/Products/components/ProductsHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ProductsHeader = () => {
+  return (
+    <div className="products-header">
+      <div className="products-header-wrapper">
+        <p>Купи комбо</p>
+        <p>и ВЫИГРАЙ CYBERPUNK 2077 </p>
+        <a href="*" className="index-header__btn">
+          КУПИТЬ КОМБО
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsHeader;
