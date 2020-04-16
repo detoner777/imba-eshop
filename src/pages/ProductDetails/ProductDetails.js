e75bc7c8bbb0f9ff70070e57854b2f914beee503
Alexey_Lisenko <detoner777@gmail.com>
2020-04-16 20:30:36 +0300

    ProductDetails layout and color displayment

--- src/pages/ProductDetails/ProductDetails.js
+++ src/pages/ProductDetails/ProductDetails.js
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
 import { Redirect } from "react-router-dom";
-import ReactImageMagnify from "react-image-magnify";
 
 import Spinner from "react-bootstrap/Spinner";
 
@@ -26,7 +25,7 @@ const ProductDetails = ({
 }) => {
   const { product, isLoading, error } = productDetails;
 
-  //did mount
+  //DidMount
   useEffect(() => {
     loadProduct(match.params.id);
   }, []);
@@ -61,70 +60,41 @@ const ProductDetails = ({
 
   return (
     product && (
-      <div className="card mb-3">
-        <div className="row no-gutters">
-          <aside className="col-sm-5 border-right">
-            <div>
+      <div className="products-details__root">
+        <div className="products-details__wrapper">
+          <div className="row no-gutters">
+            <aside className="col-sm-5 border-right">
               <img
-                className="main-img d-md-none"
                 src={require(`../../static/products/${product.image}`)}
                 alt="picture"
               />
-              <ReactImageMagnify
-                {...{
-                  smallImage: {
-                    alt: product.shortDescription,
-                    isFluidWidth: true,
-                    src: require(`../../static/products/${product.image}`),
-                  },
-                  largeImage: {
-                    src: require(`../../static/products/${product.image}`),
-                    width: 1200,
-                    height: 1200,
-                  },
-                  enlargedImageContainerStyle: {
-                    zIndex: 9,
-                    backgroundColor: "white",
-                    objectFit: "cover",
-                  },
-                  enlargedImageContainerDimensions: {
-                    width: "150%",
-                    height: "120%",
-                  },
-                  className: "d-none d-md-block ",
-                }}
-              />
-            </div>
-          </aside>
-          <aside className="col-sm-7">
-            <article className="p-5">
-              <h3 className="title mb-3">{product.name}</h3>
-
-              <div className="mb-3">
-                <var className="price h3 text-success">
-                  <span className="currency">US $</span>
-                  <span className="num">{product.price.toFixed(2)}</span>
-                </var>
-              </div>
-              {/* <dl>
-                <dt>Description</dt>
-                <dd>
-                  <p>{product.description}</p>
-                </dd>
-              </dl>
-              <dl className="row">
-                <dt className="col-sm-3">Model#</dt>
-                <dd className="col-sm-9">{product.modelNum}</dd>
-
-                <dt className="col-sm-3">Color</dt>
-                <dd className="col-sm-9">{product.color}</dd>
-
-                <dt className="col-sm-3">Delivery</dt>
-                <dd className="col-sm-9">{product.delivery}</dd>
-              </dl> */}
-
-              <hr />
-              {/* <div className="row">
+            </aside>
+            <aside className="col-sm-7">
+              <article className="p-5">
+                <h3 className="title mb-3">{product.name}</h3>
+                <div className="mb-3">
+                  <var className="price h3 text-success">
+                    <span className="num">
+                      {product.price.toFixed(0)}
+                      {` `}
+                    </span>
+                    <span className="currency">₽</span>
+                  </var>
+                </div>
+                <dl>
+                  <dt>Описание</dt>
+                  <dd>
+                    <p>{product.description}</p>
+                  </dd>
+                </dl>
+                <dl className="row">
+                  <dt className="col-sm-3">Код</dt>
+                  <dd className="col-sm-9">{product.modelNum}</dd>
+                  <dt className="col-sm-3">Цвет</dt>
+                  <dd className={`product-color-${product.color}`}></dd>
+                </dl>
+                <hr />
+                {/* <div className="row">
                 <div className="col-sm-5">
                   <dl className="dlist-inline">
                     <dt>Weight: </dt>
@@ -142,20 +112,21 @@ const ProductDetails = ({
                   </dl>
                 </div>
               </div> */}
-              <hr />
-              <button
-                onClick={toggleAddProduct}
-                className={
-                  !isAdded()
-                    ? "btn  btn-outline-primary"
-                    : "btn  btn-outline-danger"
-                }
-              >
-                <i className="fa fa-shopping-cart"></i>{" "}
-                {!isAdded() ? "Add to Cart" : "Added to Cart"}
-              </button>
-            </article>
-          </aside>
+
+                <button
+                  onClick={toggleAddProduct}
+                  className={
+                    !isAdded()
+                      ? "btn  btn-outline-primary"
+                      : "btn  btn-outline-danger"
+                  }
+                >
+                  <i className="fa fa-shopping-cart"></i>{" "}
+                  {!isAdded() ? "Add to Cart" : "Added to Cart"}
+                </button>
+              </article>
+            </aside>
+          </div>
         </div>
       </div>
     )
