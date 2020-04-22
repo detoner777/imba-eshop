db0b3c32af7435f1a50c56bb38f7b01d5a9d3c1c
Alexey_Lisenko <detoner777@gmail.com>
2020-04-22 12:07:39 +0300

    Product mob layout

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -3,8 +3,6 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroller";
 
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Spinner from "react-bootstrap/Spinner";
 
 import Product from "../../components/Product";
